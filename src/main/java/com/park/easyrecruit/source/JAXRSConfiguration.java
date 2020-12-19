56e71cebad603ed43889176f8be4210051fc7c08
Radu <radu.moldovan@ulbsibiu.ro>
2020-12-19 15:47:39 +0200

    New netbeans project

---
+++ src/main/java/com/park/easyrecruit/source/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.park.easyrecruit.source;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
