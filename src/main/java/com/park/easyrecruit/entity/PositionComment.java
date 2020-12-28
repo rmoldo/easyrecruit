417ae1649fd9b3bcb8c352f6e464a3f514bb71b7
Tallarry <silvantalos@ymail.com>
2020-12-28 22:04:33 +0200

    Add Position and PositionComment entities
    WIP

---
+++ src/main/java/com/park/easyrecruit/entity/PositionComment.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.park.easyrecruit.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Silvan
+ */
+@Entity
+@Table(name = "PositionComment")
+public class PositionComment implements Serializable {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Position position;
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    //@ManyToOne
+    //private int positionId;
+    private int userId;
+    private String text;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /*
+    public int getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(int positionId) {
+        this.positionId = positionId;
+    }
+*/
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+    
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof PositionComment )) return false;
+        return id != null && id.equals(((PositionComment) object).getId());
+    }
+
+    @Override
+    public String toString() {
+        return "PositionComment{" + "position=" + position + ", id=" + id + ", userId=" + userId + ", text=" + text + '}';
+    }
+
+    
+}
