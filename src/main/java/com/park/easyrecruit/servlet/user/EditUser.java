ed31b1ef68330d333a9e4a1f28b697633f2a1087
Radu <radu.moldovan@ulbsibiu.ro>
2021-01-02 14:55:12 +0200

    Auto stash before rebase of "master"

---
+++ src/main/java/com/park/easyrecruit/servlet/user/EditUser.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.park.easyrecruit.servlet.user;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author M Radu
+ */
+@WebServlet(name = "EditUser", urlPatterns = {"/Users/Edit"})
+public class EditUser extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "EasyRecruit:EditUser:servlet";
+    }
+}
